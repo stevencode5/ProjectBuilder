176a0503a52949c4142255990310f8c2ff29b32b
Steven <jsgonzalez@vectoritcgroup.com>
2018-06-06 17:49:12 -0500

    Se agrega modelo de modulo para manejo de modulos dependientes

---
+++ src/main/java/com/mycompany/projectbuilder/Modulo.java
@@ -0,0 +1,31 @@
+package com.mycompany.projectbuilder;
+
+import java.util.List;
+
+/**
+ *
+ * @author JSGonzalez
+ */
+public class Modulo {
+
+    private final String nombre;
+
+    private List<Modulo> nodosDependientes;
+
+    public Modulo(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public List<Modulo> getNodosDependientes() {
+        return nodosDependientes;
+    }
+
+    public void setNodosDependientes(List<Modulo> nodosDependientes) {
+        this.nodosDependientes = nodosDependientes;
+    }
+
+}
